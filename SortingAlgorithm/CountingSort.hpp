c5e7aa45b2845966389b23b9f61d90c27c67bb94
giannakat <gkcarreon@gmail.com>
2025-03-16 17:50:23 +0800

    stable counting sort

--- SortingAlgorithm/CountingSort.hpp
+++ SortingAlgorithm/CountingSort.hpp
@@ -34,4 +34,63 @@ class CountingSort:public Sortable{
         // Clean up dynamically allocated memory
         delete[] count;
     }
+
+    void sort1(int arr[], int size){
+        int maxValue = arr[0];
+
+        for(int i = 1; i < size; i++){
+            if(arr[i]>maxValue){
+                maxValue = arr[i];
+            }
+        }
+
+        int count[size+1] = {0};
+
+        for(int i = 0; i < size; i++){
+            count[arr[i]]++;
+        }
+
+        int index = 0;
+        for(int i = 0; i <= maxValue; i++){
+            if(count[i] > 0){
+                arr[index++] = i;
+                count[i]--;
+            }
+        }
+    }
+
+    //stable sort
+    void stableCountSort(int arr[], int size){
+        int maxValue = arr[0];
+        int output[size];
+
+        for(int i = 0; i < size; i++){
+            if(maxValue < arr[i]){
+                maxValue = arr[i];
+            }
+        }
+
+        int count[size+1] = {0};
+        
+        //count occurence 
+        for(int i = 0; i < size; i++){
+            count[arr[i]]++;
+        }
+
+        //prefix sum
+        for(int i = 1; i < size; i++){
+            count[i] += count[i-1];
+        }
+
+        //build output   
+        for(int i = 0; i < size; i++){
+            output[count[arr[i]]-1] = arr[i];
+            count[arr[i]]--;
+        }
+
+        for(int i = 0; i < n; i++){
+            arr[i] = output[i];
+        }
+
+    }
 };
